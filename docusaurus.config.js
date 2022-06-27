5336b06eae8b935f8e4919ea462257bc79e6229b
Oshini Nugapitiya <nugapiti_im17051@stu.kln.ac.lk>
2022-06-27 14:07:59 +0530

    url changed

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "My Site",
   tagline: "Dinosaurs are cool",
-  url: "coruscating-youtiao-efc67e.netlify.app",
+  url: "https://coruscating-youtiao-efc67e.netlify.app/",
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
